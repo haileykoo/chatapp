eb970d6873ebed855a0e09ba3400af9b20811815
haileykoo <haileykwon8@gmail.com>
2020-04-26 15:11:53 -0700

    React fixed

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import ChatApp from './App';
+
+ReactDOM.render(<ChatApp />, document.getElementById('root'));
+
+
